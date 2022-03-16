9269f3d8c0566742211c19fa2ae7b43b03f41e84
RB-Kinane <mkinane@gmail.com>
2022-03-16 14:21:16 +0100

    P3

--- Jenkins-test/Jenkins-test/Controllers/WeatherForecastController.cs
+++ Jenkins-test/Jenkins-test/Controllers/WeatherForecastController.cs
@@ -26,14 +26,26 @@ namespace Jenkins_test.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            try
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                string login = "admin";
+                string password = "administrator";
+                string sql = "SELECT * FROM Users where login = '"+ login + "' and password = '"+ password + "'";
+                var rng = new Random();
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)],
+                    Query = sql
+                })
+                .ToArray();
+            }
+            catch (Exception)
+            {
+                _logger.LogError("Error");
+                throw;
+            }
         }
     }
 }
--- Jenkins-test/Jenkins-test/WeatherForecast.cs
+++ Jenkins-test/Jenkins-test/WeatherForecast.cs
@@ -12,6 +12,6 @@ namespace Jenkins_test
 
         public string Summary { get; set; }
 
-        public string Password => "$ssqs$ùsdf";
+        public string Query { get; set; }
     }
 }
