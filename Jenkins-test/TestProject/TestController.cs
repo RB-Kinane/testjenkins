9701490d6e3e8ac50dc336b6438851926a056d44
RB-Kinane <mkinane@gmail.com>
2022-03-17 14:23:12 +0100

    Add Test Project

---
+++ Jenkins-test/TestProject/TestController.cs
@@ -0,0 +1,37 @@
+using Jenkins_test;
+using System;
+using Xunit;
+
+namespace TestProject
+{
+    public class TestController
+    {
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var primeService = new PrimeService();
+            bool result = primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+
+        //[Fact]
+        //public void IsPrime_InputIs0_ReturnFalse()
+        //{
+        //    var primeService = new PrimeService();
+        //    bool result = primeService.IsPrime(0);
+
+        //    Assert.False(result, "0 should be prime");
+        //}
+
+        //[Fact]
+        //public void IsPrime_InputIsMinus1_ReturnFalse()
+        //{
+        //    var primeService = new PrimeService();
+        //    bool result = primeService.IsPrime(-1);
+
+        //    Assert.False(result, "-1 should be prime");
+        //}
+
+    }
+}
