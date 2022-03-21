36a4c5903f9235552fff951881682734265be68f
RB-Kinane <mkinane@gmail.com>
2022-03-21 12:32:53 +0100

    up

--- Jenkins-test/TestProject/TestController.cs
+++ Jenkins-test/TestProject/TestController.cs
@@ -15,23 +15,5 @@ namespace TestProject
             Assert.False(result, "1 should not be prime");
         }
 
-        //[Fact]
-        //public void IsPrime_InputIs0_ReturnFalse()
-        //{
-        //    var primeService = new PrimeService();
-        //    bool result = primeService.IsPrime(0);
-
-        //    Assert.False(result, "0 should be prime");
-        //}
-
-        //[Fact]
-        //public void IsPrime_InputIsMinus1_ReturnFalse()
-        //{
-        //    var primeService = new PrimeService();
-        //    bool result = primeService.IsPrime(0);
-
-        //    Assert.False(result, "-1 should be prime");
-        //}
-
     }
 }
